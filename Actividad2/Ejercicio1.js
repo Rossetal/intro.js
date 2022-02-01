7cae67a05c904da07e87260b437839e581544dc5
@Rossetal <uh1hmx@yahoo.com>
2022-02-01 15:16:07 -0600

    se agregaron datos

---
+++ Actividad2/Ejercicio1.js
@@ -0,0 +1,105 @@
+//## Cuestionario de Coercion
+//#### Nombre: __Miguel Castro_____________________
+
+//1. Qué retona la siguiente expresión?
+//
+//var sum = 3 + 2
+//console.log(sum)
+
+
+> _tu respuesta_ da el resultado 5
+
+> _resultado en la consola_5
+
+//2. Qué retona la siguiente expresión?
+//```
+//typeof(3)
+//```
+
+> _tu respuesta_ number
+
+> _resultado en la consola_'number'
+
+//3. Qué retona la siguiente expresión?
+//```
+//typeof(3) === typeof("3")
+//```
+
+> _tu respuesta_false
+
+> _resultado en la consola_false
+
+//4. Qué retona la siguiente expresión?
+//```
+//5 / 0
+//```
+
+> _tu respuesta_undefined
+
+> _resultado en la consola_Infinity
+
+//5. Qué retona la siguiente expresión?
+//```
+//3 / "bob"
+//```
+
+> _tu respuesta_NaN
+
+> _resultado en la consola_NaN
+
+//6. Qué retona la siguiente expresión?
+//```
+//NaN === NaN
+//```
+
+> _tu respuesta_false
+
+> _resultado en la consola_False
+
+//7. Qué retona la siguiente expresión?
+//```
+
+
+
+
+
+//7. Qué retona la siguiente expresión?
+//```
+//typeof(NaN)
+//```
+
+> _tu respuesta_number
+
+> _resultado en la consola_Number
+
+//8. Cual es el valor de la siguiente expresion?
+//```
+//var hello
+//```
+
+> _tu respuesta_undefined
+
+> _resultado en la consola_undefined
+
+//9. Qué retona la siguiente expresión?
+//```
+//"some" + " person"
+//```
+
+> _tu respuesta_"semepersone"
+
+> _resultado en la consola_"some person"
+
+//10. Qué retona la siguiente expresión?
+//```
+//var sport; 
+//sport === undefined
+//```
+
+> _tu respuesta_undefined
+
+> _resultado en la consola_true
+
+
+
+
--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,5 +1,5 @@
-var ladoMayor = 8
-var ladoMenor = 6
+var ladoA = 8
+var ladoD = 6
 var division = 2
 var resultado = ladoMayor * ladoMenor / division
 console.log(resultado)
